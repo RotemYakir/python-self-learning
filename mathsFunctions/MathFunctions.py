1b9eb9e41019d10e25a8e38314ff335a88ea8ead
Rotem Yakir <ryakir97@gmail.com>
2024-01-09 18:12:50 +0200

    project

---
+++ mathsFunctions/MathFunctions.py
@@ -0,0 +1,15 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))  # how far the number is away from 0 (absolute value)
+print(pow(pi, 2))  # power function
+print(math.sqrt(420))  # square root
+print(max(x,y,z))
+print(min(x,y,z))
