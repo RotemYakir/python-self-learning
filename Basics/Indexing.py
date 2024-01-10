afe876db6e5cb292086f5371a45c47543bc3ec78
Rotem Yakir <ryakir97@gmail.com>
2024-01-10 15:24:42 +0200

    changes

---
+++ Basics/Indexing.py
@@ -0,0 +1,15 @@
+# index operator [] = gives access to a sequence's element (str,list,tuples)
+
+name = "rotem Yakir!"
+
+# if name[0].islower():
+#     name = name.capitalize()
+# print(name)
+
+first_name=name[:5].upper()
+last_name=name[6:11].lower()
+last_character =name[-1]
+# last_character =name[-2] # r
+print(first_name)
+print(last_name)
+print(last_character)
