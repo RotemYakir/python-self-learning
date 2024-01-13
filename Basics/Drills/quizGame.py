6ba464c11534d09c0d41c3d3ef219cca06603210
Rotem Yakir <ryakir97@gmail.com>
2024-01-13 19:54:29 +0200

    changes

---
+++ Basics/Drills/quizGame.py
@@ -0,0 +1,72 @@
+# --------------------------
+def new_game():
+    guesses = []
+    correct_answers = 0
+    question_num = 1
+
+    for key in questions:
+        print("-----------------------")
+        print(key)
+        for i in options[question_num - 1]:
+            print(i)
+        guess = input("Enter (A, B, C, or D)").upper()
+        guesses.append(guess)
+
+        correct_answers += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_answers, guesses)
+    play_again()
+
+
+# --------------------------
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG")
+        return 0
+
+
+# --------------------------
+def display_score(correct_answers, guesses):
+    print("--------------------------")
+    print("RESULTS:")
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_answers/len(questions))*100)
+    print("Your score is: "+str(score)+"%")
+
+# --------------------------
+def play_again():
+    response = input("Do you want to play again? (yes/no): ").lower()
+    if response=="yes":
+        new_game()
+    else:
+        print("Bye!!!")
+
+
+# --------------------------
+
+questions = {
+    "Who created Python?: ": "A",
+    "What year was Python created?: ": "B",
+    "Python is tributed to which comedy group?": "C",
+    "Is earth round?: ": "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates", "D. Mark Zuckerburg"],
+           ["A. 1989", "B. 1991", "C. 2000", "D. 2016"],
+           ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+           ["A. True", "B. False", "C. Sometimes", "D. What's Earth"]]
+
+new_game()
