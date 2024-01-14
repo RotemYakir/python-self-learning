54e81130e8335925244295e2e898b41503f8c143
Rotem Yakir <ryakir97@gmail.com>
2024-01-14 11:50:57 +0200

    changes

--- Basics/OOP/ClassVariables/main.py
+++ Basics/OOP/ClassVariables/main.py
@@ -3,4 +3,7 @@ from car import Car
 car_1 = Car("Chevy", "Corvette", 2021, "blue")
 car_2 = Car("Ford", "Mustang", 2022, "red")
 
-
+Car.wheels = 2
+print(car_2.wheels)
+print(car_1.wheels)
+print(Car.wheels)
