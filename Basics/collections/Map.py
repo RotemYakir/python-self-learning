5de4884897b6ce117d3a6c8156af0f771d6a498c
Rotem Yakir <ryakir97@gmail.com>
2024-01-15 21:40:17 +0200

    changes

--- Basics/collections/Map.py
+++ Basics/collections/Map.py
@@ -1,4 +1,5 @@
 # map() = applies a function to each item in an iterable (list, tuple, etc.)
+
 # map(function,iterable)
 
 store = [("shirt", 20.00),
@@ -9,3 +10,9 @@ store = [("shirt", 20.00),
 to_euros = lambda data: (data[0], data[1] * 0.82)
 # to_dollars = lambda data: (data[0], data[1] / 0.82)
 
+# store_euros = list(map(to_euros, store))
+store_euros = map(to_euros, store)
+# store_dollars = map(to_dollars, store)
+
+for i in store_euros:
+    print(i)
