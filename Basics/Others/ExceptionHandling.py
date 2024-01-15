ed50970ac671f600bfb7d6d7b4c0fc7caaa29842
Rotem Yakir <ryakir97@gmail.com>
2024-01-15 17:31:50 +0200

    changes

--- Basics/Others/ExceptionHandling.py
+++ Basics/Others/ExceptionHandling.py
@@ -1,4 +1,4 @@
-# exception = events detected during execution that interrupt the flow of a program
+# exceptions = events detected during execution that interrupt the flow of a program
 
 try:
     numerator = int(input("Enter a number to divide: "))
