b5d0ed11ed3c41b5db2fe8d98a8e536bf9536ec3
Rotem Yakir <ryakir97@gmail.com>
2024-01-17 19:04:03 +0200

    changes

--- Basics/collections/ListComprehensions.py
+++ Basics/collections/ListComprehensions.py
@@ -4,8 +4,17 @@
 #                      list = [expression for item in iterable if conditional]
 #                      list = [expression if/else for item in iterable]
 
-students = [100, 90, 80, 70, 60, 50, 40, 30, 0]
 
+# squares = []
+# for i in range(1,11):
+#     squares.append(i*i)
+# print(squares)
+squares = [i*i for i in range(1,11)]
+print(squares)
+
+print()
+
+students = [100, 90, 80, 70, 60, 50, 40, 30, 0]
 # passed_students = list(filter(lambda x: x >= 60, students))
 # passed_students = [i for i in students if i >= 60]
 
